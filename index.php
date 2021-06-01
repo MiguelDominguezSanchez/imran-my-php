708a5bca7528a69d87b369c42aba45510991c4a1
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-06-01 18:55:26 +0200

    php traits tutorial

--- index.php
+++ index.php
@@ -3,9 +3,36 @@
 // include_once 'includes/Person.php';
 // include_once 'includes/Student.php';
 
-spl_autoload_register( function ( $class ) {
-    include 'includes/' . $class . '.php';
-} );
+// spl_autoload_register( function ( $class ) {
+//     echo $class;
+//     include 'includes/' . $class . '.php';
+// } );
 
-new Student();
-new Person();
+// new Student();
+// new Person();
+
+//*************** traits *****************/
+
+trait Say_World {
+    public function say_hello() {
+        echo 'Hello Trait';
+    }
+}
+
+class Teacher {
+    public function say_name() {
+        echo 'Teacher';
+    }
+}
+
+class Base extends Teacher {
+    use Say_World;
+
+    public function __construct() {
+        //
+    }
+}
+
+$base = new Base();
+$base->say_hello();
+$base->say_name();
