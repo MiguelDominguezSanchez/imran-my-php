fcd2c9a4a4173e7802acf6def01434187870582d
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-06-02 07:16:50 +0200

    #14 traits in php

--- index.php
+++ index.php
@@ -13,26 +13,78 @@
 
 //*************** traits *****************/
 
-trait Say_World {
-    public function say_hello() {
-        echo 'Hello Trait';
+// trait Say_World {
+//     public function say_hello() {
+//         echo 'Hello Trait';
+//     }
+// }
+
+// class Teacher {
+//     public function say_name() {
+//         echo 'Teacher';
+//     }
+// }
+
+// class Base extends Teacher {
+//     use Say_World;
+
+//     public function __construct() {
+//         //
+//     }
+// }
+
+// $base = new Base();
+// $base->say_hello();
+// $base->say_name();
+
+?>
+
+<!-- //*************** #14 traits in php *****************/ -->
+
+<?php
+
+trait Singleton {
+
+    public static function get_instance() {
+        static $instance = [];
+
+        $called_class = get_called_class();
+
+        if ( ! isset( $instance[ $called_class ] ) ) {
+            echo 'Hello';
+            $instance[ $called_class ] = new $called_class();
+        }
+
+        return $instance[ $called_class ];
     }
+
 }
 
-class Teacher {
-    public function say_name() {
-        echo 'Teacher';
-    }
-}
-
-class Base extends Teacher {
-    use Say_World;
+class User {
+    use Singleton;
 
     public function __construct() {
-        //
+        // echo 'User',
     }
 }
 
-$base = new Base();
-$base->say_hello();
-$base->say_name();
+$user_one = User::get_instance();
+$user_two = User::get_instance();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
